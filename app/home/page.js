be1707a771d1ec64bf1faff9d3321e8b8701d56d
Lucassan4225 <145424551+Lucassan4225@users.noreply.github.com>
2025-02-13 12:57:00 +0700

    testing

--- app/home/page.js
+++ app/home/page.js
@@ -3,14 +3,13 @@ import React, { useEffect } from "react";
 import Layout from "@/components/ui/layout"; // Import the Layout component
 import { useSession, signIn } from "next-auth/react"; // Import session and signIn
 import { useRouter } from "next/navigation";
-
 import Post from "@/components/ui/post";
 import CreatePost from "@/components/ui/createPost"; // Import the Post component
 
 const HomePage = () => {
   const { data: session, status } = useSession();
   const router = useRouter();
-
+  
   useEffect(() => {
     if (status === "unauthenticated") {
       router.push("/"); // Redirect to sign-in if not authenticated
