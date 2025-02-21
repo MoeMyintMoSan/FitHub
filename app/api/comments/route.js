9719521c2f8a7dc15e6ca7d24109bdc249394b3b
Thet Myat Noe Thwin <u6530213@au.edu>
2025-02-22 00:11:29 +0700

    queries collected

--- app/api/comments/route.js
+++ app/api/comments/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import sql from "@/api/db/config";
-import { findUserByEmail } from "../db/queries";
+import {fetchCommentsByPostId, createComment } from "../db/queries";
 // GET Comments for a specific post
 export async function GET(req) {
   try {
@@ -11,15 +11,9 @@ export async function GET(req) {
       return NextResponse.json({ message: "Post ID is required" }, { status: 400 });
     }
 
-    const comments = await sql`
-      SELECT c.comment_id, c.content, c.post_id, u.user_id, u.user_name
-      FROM "Comment" c
-      JOIN users u ON c.user_id = u.user_id
-      WHERE c.post_id = ${post_id}
-      ORDER BY c.comment_id DESC;
-    `;
-
+    const comments = await fetchCommentsByPostId(post_id);
     return NextResponse.json(comments, { status: 200 });
+
   } catch (error) {
     console.error("Error fetching comments:", error);
     return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
@@ -31,23 +25,9 @@ export async function POST(req) {
   try {
     const { email, post_id, content } = await req.json();
 
-    if (!email || !post_id || !content) {
-      return NextResponse.json({ message: "All fields are required" }, { status: 400 });
-    }
+    const comment = await createComment(email, post_id, content);
+    return NextResponse.json(comment, { status: 201 });
 
-    const user = await findUserByEmail(email);
-    const user_id = user.user_id;
-
-    const result = await sql`
-      INSERT INTO "Comment" (user_id, post_id, content)
-      VALUES (${user_id}, ${post_id}, ${content})
-      RETURNING comment_id, content, post_id, user_id;
-    `;
-
-    const comment = result[0];
-    comment.user_name = user.user_name
-    
-    return NextResponse.json({ message: "Comment added", comment: result[0] }, { status: 201 });
   } catch (error) {
     console.error("Error adding comment:", error);
     return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import sql from "../db/config";
-import { findUserByEmail } from "../db/queries";
+import { PostCreate } from "../db/queries";
 
 
 export async function GET(req) {
@@ -19,47 +19,12 @@ export async function GET(req) {
   }
 }
 export async function POST(req) {
+  const formData = await req.formData();
+    
   try {
-    const formData = await req.formData();
-    const email = formData.get("email");
-    const title = formData.get("title");
-    const content = formData.get("content");
-    const post_visibility = formData.get("post_visibility");
-    const image = formData.get("image");
-    const details = JSON.parse(formData.get("details"));
-
-
-    const user = await findUserByEmail(email);
-    const user_id = user.user_id;
-    const content_type = user.user_type;
-
-    // Insert into Post table
-    const postResult = await sql`
-      INSERT INTO Post (professional_id, title, content, image, content_type, post_visibility)
-      VALUES (${user_id}, ${title}, ${content}, ${image}, ${content_type}, ${post_visibility})
-      RETURNING post_id;
-    `;
-
-    const postId = postResult[0].post_id;
-
-    // Insert into corresponding detail table
-    if (content_type === "Trainer") {
-      for (const detail of details) {
-        await sql`
-          INSERT INTO trainer_post_details (post_id, exercise, reps, sets)
-          VALUES (${postId}, ${detail.exercise}, ${detail.reps}, ${detail.sets});
-        `;
-      }
-    } else if (content_type === "Nutritionist") {
-      for (const detail of details) {
-        await sql`
-          INSERT INTO nutritionist_post_details (post_id, food, calories)
-          VALUES (${postId}, ${detail.food}, ${detail.calories});
-        `;
-      }
-    }
-
-    return NextResponse.json({ message: "Post created successfully" }, { status: 200 });
+    const post = await PostCreate(formData);
+    return NextResponse.json(post, { status: 201 });
+    
   } catch (error) {
     console.error("Error creating post:", error);
     return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
